ab23210fee46d56219c58966be678c270cb9064d
GaganDeep Tomar <gagandeep280597@gmail.com>
2018-10-14 11:52:32 +0530

    final

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_060502) do
+ActiveRecord::Schema.define(version: 2018_10_14_061419) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2018_10_14_060502) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
