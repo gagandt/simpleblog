eb496bdcb44465ea0f305b96ac83fe3f836c48d4
GaganDeep Tomar <gagandeep280597@gmail.com>
2018-10-14 11:44:36 +0530

    Scaffolded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_13_144041) do
+ActiveRecord::Schema.define(version: 2018_10_14_060502) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
